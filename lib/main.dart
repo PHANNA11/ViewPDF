15a6a84dac6d7498d10e9d26deef040de868827e
PHANNA11 <phaphanna2018@gmail.com>
2022-09-29 08:55:10 +0700

    desktop minimun size

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_pdf/screen/controller_screen_device.dart';
 import 'package:flutter_pdf/screen/phoneApp/pdfscreen_phone.dart';
+import 'package:window_size/window_size.dart';
 
 void main(List<String> args) {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('រៀនCode បែកស្លុយ');
+    setWindowMinSize(const Size(400, 720));
+    setWindowMaxSize(Size.infinite);
+  }
   runApp(const MyApp());
 }
 
